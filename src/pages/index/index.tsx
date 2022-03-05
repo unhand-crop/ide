b453e6787aca7819ba169d5af4f57dd113a8311d
RoyLin <1002591652@qq.com>
2022-03-05 20:15:45 +0800

    feat: init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import styles from "./index.module.scss";
+
+function Index() {
+  return <div className={styles.container}>Test</div>;
+}
+
+export default Index;
---
+++ webpack.renderer.config.js
@@ -0,0 +1,36 @@
+const rules = require("./webpack.rules");
+const plugins = require("./webpack.plugins");
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+});
+
+rules.push({
+  test: /\.scss$/,
+  exclude: /\.module.scss$/,
+  use: [
+    { loader: "style-loader" },
+    { loader: "css-loader" },
+    { loader: "sass-loader" },
+  ],
+});
+
+rules.push({
+  test: /\.module.scss$/,
+  use: [
+    { loader: "style-loader" },
+    { loader: "css-loader" },
+    { loader: "sass-loader" },
+  ],
+});
+
+module.exports = {
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".scss"],
+  },
+};
