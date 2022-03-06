d08f54efbebec6d3bebcf37ca156ca30ae9c248b
Raozhi <2829656235@qq.com>
2022-03-06 18:41:06 +0800

    feat: add search extension

--- src/pages/Workbench/extensions/index.ts
+++ src/pages/Workbench/extensions/index.ts
@@ -1,8 +1,10 @@
 import { IExtension } from "@dtinsight/molecule/esm/model";
 import { LauncherExtension } from "./launcher";
+import { SearchExtension } from "./search";
 
 const extensions: IExtension[] = [
-	new LauncherExtension()
+	new LauncherExtension(),
+	new SearchExtension()
 ];
 
 export default extensions;
