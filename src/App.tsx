5c1152d8fd5727a43f9b66040c96b0b85e69e5ac
RoyLin <1002591652@qq.com>
2022-03-07 12:07:47 +0800

    feat: signalR

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as signalR from "@microsoft/signalr";
 
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import Workbench from "@/pages/Workbench";
+import { useMount } from "ahooks";
 import useSettingModel from "@/models/setting";
 
 const App = () => {
   useSettingModel();
+
+  useMount(async () => {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl(
+        "http://121.41.65.155:8085/binan?symbols=btcusdt&eventName=aggTrade&ticket="
+      )
+      .build();
+
+    connection.on("binanMessage", (event, symbol, message) => {
+      console.log(message);
+    });
+
+    // await connection.start();
+  });
 
   return (
     <Router>
