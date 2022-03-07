3990f84922925ed9c482213062423b666a02c87d
Raozhi <2829656235@qq.com>
2022-03-07 13:11:32 +0800

    feat: add market extensions

---
+++ src/pages/Workbench/extensions/market/base.tsx
@@ -0,0 +1,34 @@
+import {
+  IActivityBarItem,
+  IEditorTab,
+  ISidebarPane,
+} from "@dtinsight/molecule/esm/model";
+//   import MarketSidebarView from "./MarketSidebarView";
+//   import ResultPaneView from "../result/ResultPaneView";
+import molecule from "@dtinsight/molecule";
+
+export const marketActivityBar: IActivityBarItem = {
+  id: "sidebar.market",
+  name: "MarketActivityBar",
+  sortIndex: 1,
+  title: "Market Management",
+  icon: "database",
+};
+
+export const marketSidebar: ISidebarPane = {
+  id: "sidebar.market",
+  title: "Market Pane",
+  // render: () => {
+  // //   return <MarketSidebarView />;
+  // },
+};
+
+export function openCreateDataSourceView(name: string) {
+  molecule.editor.open({
+    id: name,
+    name,
+    renderPane: () => {
+      // return <ResultPaneView />;
+    },
+  });
+}
