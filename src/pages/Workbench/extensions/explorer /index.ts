4bd723642485df81a3a6400293506aaf531481fb
Raozhi <2829656235@qq.com>
2022-03-07 17:47:02 +0800

    feat: add explorer extensions

---
+++ src/pages/Workbench/extensions/explorer /index.ts
@@ -0,0 +1,17 @@
+import molecule from "@dtinsight/molecule";
+import { IExtension } from "@dtinsight/molecule/esm/model/extension";
+import { IExtensionService } from "@dtinsight/molecule/esm/services";
+
+export class ExplorerExtension implements IExtension {
+  id: string = ExplorerExtension.name;
+  name: string = ExplorerExtension.name;
+
+  activate(extensionCtx: IExtensionService): void {
+    molecule.explorer.removePanel("outline");
+    molecule.explorer.removeAction("sidebar.explore.actionDesc");
+  }
+
+  dispose(extensionCtx: IExtensionService): void {
+    throw new Error("Method not implemented.");
+  }
+}
--- src/pages/Workbench/extensions/index.ts
+++ src/pages/Workbench/extensions/index.ts
@@ -3,12 +3,14 @@ import { LauncherExtension } from "./launcher";
 import { SearchExtension } from "./search";
 import { AccountExtension } from "./account";
 import { MarketExtension } from "./market";
+import { ExplorerExtension } from "./explorer ";
 
 const extensions: IExtension[] = [
 	new LauncherExtension(),
 	new SearchExtension(),
 	new AccountExtension(),
-	new MarketExtension()
+	new MarketExtension(),
+	new ExplorerExtension()
 ];
 
 export default extensions;
