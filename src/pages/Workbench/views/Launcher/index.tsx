87590ec1e8c3b7324bf2b0e0735d388772b2e4b9
Raozhi <2829656235@qq.com>
2022-03-10 14:21:52 +0800

    feat: new algorithm pop-up

--- src/pages/Workbench/views/Launcher/index.tsx
+++ src/pages/Workbench/views/Launcher/index.tsx
@@ -1,16 +1,15 @@
 import Button from "./components/Button";
-// import useEditorModel from "@/models/editor";
+import { IconAdd } from "@/components/iconfont";
+import Modal from "@/components/modal";
+import { Pagination } from "antd";
 import React from "react";
 import styles from "./index.module.scss";
+import { useReactive } from "ahooks";
 
 const Launcher = () => {
-  //   const { setDirPath } = useEditorModel();
-
-  //   const createNew = async () => {
-  //     const path = await window.api.local.getDirectory();
-  //     await window.api.engine.create(path);
-  //     setDirPath(path);
-  //   };
+  const state = useReactive({
+    visible: false,
+  });
 
   const openPro = async () => {
     await window.api.local.openDirectory();
@@ -21,10 +20,34 @@ const Launcher = () => {
       <div className={styles.project}>
         <p className={styles.title}>启动</p>
         <div className={styles.project_button}>
-          <Button title="新建算法" />
+          <Button title="新建算法" onClick={() => (state.visible = true)} />
           <Button onClick={() => openPro()} title="打开算法" />
         </div>
       </div>
+      <Modal
+        title="新建算法"
+        visible={state.visible}
+        onCancel={() => (state.visible = false)}
+      >
+        <div className={styles.algorithm_container}>
+          <ul className={styles.algorithm_list}>
+            <li className={styles.algorithm_item}>
+              <div className={styles.new_algorithm}>
+                <IconAdd size={80} color="#b2b5be" />
+                <p>新建</p>
+              </div>
+            </li>
+            <li className={styles.algorithm_item}></li>
+            <li className={styles.algorithm_item}></li>
+            <li className={styles.algorithm_item}></li>
+            <li className={styles.algorithm_item}></li>
+            <li className={styles.algorithm_item}></li>
+            <li className={styles.algorithm_item}></li>
+            <li className={styles.algorithm_item}></li>
+          </ul>
+          <Pagination />
+        </div>
+      </Modal>
     </div>
   );
 };
