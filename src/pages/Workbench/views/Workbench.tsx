f083f679a2a5e128d7d18c8a56ddd27f4b37a2bb
RoyLin <1002591652@qq.com>
2022-03-10 17:19:43 +0800

    refactor: workbench

--- src/pages/Workbench/views/Workbench.tsx
+++ src/pages/Workbench/views/Workbench.tsx
@@ -1,6 +1,11 @@
-import "reflect-metadata";
 import "@dtinsight/molecule/esm/style/mo.css";
+import "reflect-metadata";
 
+import { Display, Pane, SplitPane } from "@dtinsight/molecule/esm/components";
+import {
+  ILayout,
+  MenuBarMode,
+} from "@dtinsight/molecule/esm/model/workbench/layout";
 import {
   ILayoutController,
   LayoutController,
@@ -8,6 +13,8 @@ import {
 import React, { useEffect } from "react";
 import {
   classNames,
+  getBEMElement,
+  getBEMModifier,
   getFontInMac,
   prefixClaName,
 } from "@dtinsight/molecule/esm/common/className";
@@ -16,18 +23,15 @@ import { APP_PREFIX } from "@dtinsight/molecule/esm/common/const";
 import { ActivityBarView } from "@dtinsight/molecule/esm/workbench/activityBar";
 import { EditorView } from "@dtinsight/molecule/esm//workbench/editor";
 import { ID_APP } from "@dtinsight/molecule/esm/common/id";
-import { ILayout } from "@dtinsight/molecule/esm/model/workbench/layout";
 import { IWorkbench } from "@dtinsight/molecule/esm/model/workbench";
 import { LayoutService } from "@dtinsight/molecule/esm/services";
-//@ts-ignore
-import Pane from "react-split-pane/lib/Pane";
-import { PanelView } from "@dtinsight/molecule/esm//workbench/panel";
-import { SidebarView } from "@dtinsight/molecule/esm//workbench/sidebar";
-import SplitPane from "react-split-pane";
-import { StatusBarView } from "@dtinsight/molecule/esm//workbench/statusBar";
+import { MenuBarView } from "@dtinsight/molecule/esm/workbench/menuBar";
+import { PanelView } from "@dtinsight/molecule/esm/workbench/panel";
+import { SidebarView } from "@dtinsight/molecule/esm/workbench/sidebar";
+// import { SidePane } from "./SidePane";
+import { StatusBarView } from "@dtinsight/molecule/esm/workbench/statusBar";
 import { connect } from "@dtinsight/molecule/esm/react";
 import { container } from "tsyringe";
-// import useSettingModel from "@/models/setting";
 import molecule from "@dtinsight/molecule";
 import useEditorModel from "@/models/editor";
 import useSettingModel from "@/models/setting";
@@ -36,6 +40,18 @@ const mainBenchClassName = prefixClaName("mainBench");
 const workbenchClassName = prefixClaName("workbench");
 const compositeBarClassName = prefixClaName("compositeBar");
 const appClassName = classNames(APP_PREFIX, getFontInMac());
+const workbenchWithHorizontalMenuBarClassName = getBEMModifier(
+  workbenchClassName,
+  "with-horizontal-menuBar"
+);
+const withHiddenStatusBar = getBEMModifier(
+  workbenchClassName,
+  "with-hidden-statusBar"
+);
+const displayActivityBarClassName = getBEMElement(
+  workbenchClassName,
+  "display-activityBar"
+);
 
 const layoutController = container.resolve(LayoutController);
 const layoutService = container.resolve(LayoutService);
@@ -65,31 +81,63 @@ function WorkbenchView(props: IWorkbench & ILayout & ILayoutController) {
     });
   }, [model.dirPath]);
 
-  const getContent = (panelMaximized: boolean, panelHidden: boolean) => {
-    const editor = (
-      <Pane
-        key="editorView"
-        initialSize={panelHidden ? "100%" : horizontalSplitPanePos[0]}
-        maxSize="100%"
-        minSize="10%"
-      >
-        <EditorView />
-      </Pane>
-    );
-
-    const panel = (
-      <Pane key="panelView">
-        <PanelView />
-      </Pane>
-    );
-
-    if (panelHidden) {
-      return editor;
+  const getSizes = () => {
+    if (panel.hidden) {
+      return ["100%", 0];
     }
-    if (panelMaximized) {
-      return panel;
+    if (panel.panelMaximized) {
+      return [0, "100%"];
     }
-    return [editor, panel];
+    return horizontalSplitPanePos;
+  };
+
+  const isMenuBarVertical =
+    !menuBar.hidden && menuBar.mode === MenuBarMode.vertical;
+  const isMenuBarHorizontal =
+    !menuBar.hidden && menuBar.mode === MenuBarMode.horizontal;
+  const horizontalMenuBar = isMenuBarHorizontal
+    ? workbenchWithHorizontalMenuBarClassName
+    : null;
+  const hideStatusBar = statusBar.hidden ? withHiddenStatusBar : null;
+  const workbenchFinalClassName = classNames(
+    workbenchClassName,
+    horizontalMenuBar,
+    hideStatusBar
+  );
+
+  const handleSideBarChanged = (sizes: number[]) => {
+    if (sidebar.hidden) {
+      const clientSize = sizes[1];
+      const sidebarSize = splitPanePos[0];
+      if (typeof sidebarSize === "string") {
+        // the sideBar size is still a default value
+        const numbSize = parseInt(sidebarSize, 10);
+        onPaneSizeChange?.([numbSize, clientSize - numbSize]);
+      } else {
+        onPaneSizeChange?.([sidebarSize, clientSize - sidebarSize]);
+      }
+    } else {
+      onPaneSizeChange?.(sizes);
+    }
+  };
+
+  const handleEditorChanged = (sizes: number[]) => {
+    if (panel.hidden) {
+      // get the non-zero size means current client size
+      const clientSize = sizes.find((s) => s)!;
+      const panelSize = horizontalSplitPanePos[1];
+      if (typeof panelSize === "string") {
+        // the editor size is still a default value
+        const editorPercent =
+          parseInt(horizontalSplitPanePos[0] as string, 10) / 100;
+        const numbericSize = clientSize * editorPercent;
+        onHorizontalPaneSizeChange?.([numbericSize, clientSize - numbericSize]);
+      } else {
+        onHorizontalPaneSizeChange?.([clientSize - panelSize, panelSize]);
+      }
+    } else {
+      onHorizontalPaneSizeChange?.(sizes);
+    }
   };
 
   return (
@@ -98,47 +146,53 @@ function WorkbenchView(props: IWorkbench & ILayout & ILayoutController) {
       className={classNames(appClassName, "myMolecule")}
       tabIndex={0}
     >
-      <div className={workbenchClassName}>
-        {/* <TitleBar /> */}
+      <div className={workbenchFinalClassName}>
+        <Display visible={isMenuBarHorizontal}>
+          <MenuBarView mode={MenuBarMode.horizontal} />
+        </Display>
         <div className={mainBenchClassName}>
           <div className={compositeBarClassName}>
-            {!activityBar.hidden && <ActivityBarView />}
+            {/* <Display visible={isMenuBarVertical}>
+              <MenuBarView mode={MenuBarMode.vertical} />
+            </Display> */}
+            <Display
+              visible={!activityBar.hidden}
+              className={displayActivityBarClassName}
+            >
+              <ActivityBarView />
+            </Display>
           </div>
           <SplitPane
+            sizes={sidebar.hidden ? [0, "100%"] : splitPanePos}
             split="vertical"
-            primary="first"
-            allowResize={true}
-            onChange={onPaneSizeChange as any}
+            allowResize={[false, true]}
+            onChange={handleSideBarChanged}
+            onResizeStrategy={() => ["keep", "pave"]}
           >
-            <Pane
-              minSize="170px"
-              initialSize={splitPanePos[0]}
-              maxSize="80%"
-              className={sidebar.hidden && "hidden"}
-            >
+            <Pane minSize={170} maxSize="80%">
               <SidebarView />
             </Pane>
             <SplitPane
-              primary="first"
+              sizes={getSizes()}
+              allowResize={[false, true]}
               split="horizontal"
-              allowResize={true}
-              // react-split-pane onChange: (newSizes: [size, ratio]) => void；
-              onChange={onHorizontalPaneSizeChange as any}
+              onChange={handleEditorChanged}
+              onResizeStrategy={() => ["pave", "keep"]}
             >
-              {getContent(!!panel.panelMaximized, !!panel.hidden)}
+              <Pane minSize="10%" maxSize="80%">
+                <EditorView />
+              </Pane>
+              <PanelView />
             </SplitPane>
-            {/* <Pane
-              minSize="40px"
-              initialSize="240px"
-              maxSize="40%"
-              className={"rightSidebar"}
-            >
-              <Sidebar current={MySidePane.id} panes={[MySidePane]} />
-            </Pane> */}
           </SplitPane>
+          {/* <div style={{ width: 300 }}>
+            <Sidebar current={SidePane.id} panes={[SidePane]} />
+          </div> */}
         </div>
       </div>
-      {!statusBar.hidden && <StatusBarView />}
+      <Display visible={!statusBar.hidden}>
+        <StatusBarView />
+      </Display>
     </div>
   );
 }
