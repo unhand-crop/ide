7f652b74242f6097d23035979a7393d82acfdf99
Raozhi <2829656235@qq.com>
2022-03-14 17:42:15 +0800

    feat: store currency list locally

--- src/pages/Workbench/extensions/market/components/AddList/index.tsx
+++ src/pages/Workbench/extensions/market/components/AddList/index.tsx
@@ -9,6 +9,12 @@ interface AddListProps {
 }
 
 const AddList = ({ item, index }: AddListProps) => {
+  const handleAdd = async (item: GetSymbolsOutput) => {
+    let addList = await window.api.store.get("defaultList");
+    addList.push(JSON.parse(JSON.stringify(item)));
+    await window.api.store.set("defaultList", addList);
+  };
+
   return (
     <li className={styles.item}>
       <div className={styles.left}>
@@ -17,7 +23,7 @@ const AddList = ({ item, index }: AddListProps) => {
       </div>
       <div className={styles.right}>
         <div className={styles.symbol_description}>{item.remark}</div>
-        <div className={styles.exchange_cell}>
+        <div className={styles.exchange_cell} onClick={() => handleAdd(item)}>
           <IconAdd size={28} color="#2c62ff" />
         </div>
       </div>
