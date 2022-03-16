20dd44c00a561844ac3eedf5e3ae055e9427bc17
Raozhi <2829656235@qq.com>
2022-03-16 15:14:35 +0800

    style: modify modal component

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -6,18 +6,19 @@ import styles from "./index.module.scss";
 interface ModalProps {
   visible: boolean;
   title: string;
+  width?: number;
   children: ReactNode;
   onCancel: () => void;
 }
 
-const Modal = ({ visible, title, children, onCancel }: ModalProps) => {
+const Modal = ({ visible, title, children, onCancel, width }: ModalProps) => {
   return (
     <ModalCom
       visible={visible}
       title={title}
       footer={false}
       centered={true}
-      width={866}
+      width={width}
       onCancel={() => onCancel()}
       className={styles.component_modal}
     >
