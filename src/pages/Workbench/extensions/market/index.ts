cb11268a60e0806dddeec99591bfdcf2a7315ae4
shitong <1275741406@qq.com>
2022-04-03 15:22:50 +0800

    fix(market/index.ts configs/index.ts): port change / delete unuse code

--- src/pages/Workbench/extensions/market/index.ts
+++ src/pages/Workbench/extensions/market/index.ts
@@ -2,7 +2,7 @@ import molecule from "@dtinsight/molecule";
 import { UniqueId } from "@dtinsight/molecule/esm/common/types";
 import { IExtension } from "@dtinsight/molecule/esm/model";
 import { IExtensionService } from "@dtinsight/molecule/esm/services";
-import { marketActivityBar, marketSidebar, algorithmActivityBar, algorithmSidebar } from "./base";
+import { marketActivityBar, marketSidebar } from "./base";
 
 export class MarketExtension implements IExtension {
   id: UniqueId = marketSidebar.id;
@@ -15,8 +15,6 @@ export class MarketExtension implements IExtension {
   initUI() {
     molecule.sidebar.add(marketSidebar);
     molecule.activityBar.add(marketActivityBar);
-    molecule.sidebar.add(algorithmSidebar);
-    molecule.activityBar.add(algorithmActivityBar);
     molecule.activityBar.onClick((id) => {
       if (id === marketSidebar.id) {
         const { panel, sidebar } = molecule.layout.getState();
@@ -27,19 +25,11 @@ export class MarketExtension implements IExtension {
           molecule.layout.toggleSidebarVisibility();
         }
       }
-      if (id === algorithmSidebar.id) {
-        // molecule.activityBar.remove(marketActivityBar.id);
-        // molecule.sidebar.remove(marketSidebar.id);
-        // molecule.activityBar.remove(algorithmSidebar.id);
-        // molecule.sidebar.remove(algorithmSidebar.id);
-      }
     });
   }
 
   dispose(extensionCtx: IExtensionService): void {
     molecule.activityBar.remove(marketActivityBar.id);
     molecule.sidebar.remove(marketSidebar.id);
-    molecule.activityBar.remove(algorithmSidebar.id);
-    molecule.sidebar.remove(algorithmSidebar.id);
   }
 }
