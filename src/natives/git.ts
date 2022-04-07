c5333852a524aa751af81f34ae9dbc0357b8a919
shitong <1275741406@qq.com>
2022-04-07 22:50:04 +0800

    feat(git.ts local.ts newalgorithmmodal/index.tsx): new algorithm

--- src/natives/git.ts
+++ src/natives/git.ts
@@ -1,29 +1,34 @@
 
 import { BrowserWindow, ipcMain, ipcRenderer } from "electron";
-import http from 'isomorphic-git/http/node';
-import git from 'isomorphic-git';
-import path from 'path';
 import fs from 'fs';
 import unzip from "unzipper";
 import request from "request";
-
+import path from "path";
 
 export const registerGitHandlers = async (mainWindow: BrowserWindow) => {
-    ipcMain.handle("gitHttp.clone", async (_, { fileName, gitUrl, gitFileName }: any) => {
+    ipcMain.handle("gitHttp.clone", async (_, { fileName, gitUrl, gitFileName, extractUrl, extractCallback }: any) => {
+        console.log("fileName, gitUrl, gitFileName, extractUrl, extractCallback", fileName, gitUrl, gitFileName, extractUrl, extractCallback);
 
-        const zip = request(gitUrl);
-        const Write = fs.createWriteStream("./a.zip");
-        zip.pipe(Write).on("close", function (err: any) {
-            const Read = fs.createReadStream("./a.zip");
-            Read.pipe(unzip.Extract({ path: './' }));
+        extractCallback && extractCallback(10);
+        const writeUrl = path.join(extractUrl, gitFileName);
+        request(gitUrl).pipe(fs.createWriteStream(writeUrl)).on("close", function (err: any) {
+            extractCallback && extractCallback(40);
+            fs.createReadStream(writeUrl).pipe(unzip.Extract({ path: extractUrl })).on("close", (err: any) => {
+                const oldName = path.join(extractUrl, path.parse(gitFileName).name);
+                const newName = path.join(extractUrl, fileName);
+                extractCallback && extractCallback(60);
+                fs.rename(oldName, newName, (err) => {
+                    extractCallback && extractCallback(100, newName);
+                })
+            })
         });
     });
 };
 
 export const registerGitInvokes = () => {
     return {
-        async clone({ fileName, gitUrl, gitFileName }: any) {
-            return await ipcRenderer.invoke("gitHttp.clone", { fileName, gitUrl, gitFileName });
+        async clone({ fileName, gitUrl, gitFileName, extractUrl, extractCallback }: any) {
+            return await ipcRenderer.invoke("gitHttp.clone", { fileName, gitUrl, gitFileName, extractUrl, extractCallback });
         },
     };
 };
