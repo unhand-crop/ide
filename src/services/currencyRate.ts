c9f85779e0cd4701f59ece34c86a7f98cabd2d40
Raozhi <2829656235@qq.com>
2022-04-11 16:17:02 +0800

    feat: add currency exchange rate function

---
+++ src/services/currencyRate.ts
@@ -0,0 +1,32 @@
+import { Response } from "./dtos";
+import request from "@/utils/request";
+
+export interface GetRealTimeCurrencyConversionInpt {
+  from: string;
+  to: string;
+}
+
+export interface GetRealTimeCurrencyConversionOutPut {
+  status: string;
+  requestId: string;
+  isSuccess: boolean;
+  converted: number;
+  from: string;
+  initialAmount: number;
+  symbol: string;
+  to: string;
+  last: {
+    askPrice: number;
+    bidPrice: number;
+    exchange: number;
+    timestamp: number;
+  };
+}
+
+export const getCurrencyRates = async (
+  data: GetRealTimeCurrencyConversionInpt
+): Promise<Response<GetRealTimeCurrencyConversionOutPut>> => {
+  return request.get("/eapi/v1/currencyrate/getcurrencyrates", {
+    params: data,
+  });
+};
