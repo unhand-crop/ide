dbce6c8ad74f2533d1926b84f231244b9c80b1a8
Raozhi <2829656235@qq.com>
2022-04-13 21:26:20 +0800

    feat: add API document search function

--- src/pages/Workbench/views/SidePane/index.tsx
+++ src/pages/Workbench/views/SidePane/index.tsx
@@ -1,36 +1,25 @@
-import { CaretRightOutlined, CloseOutlined } from "@ant-design/icons";
+import { CaretRightOutlined, SearchOutlined } from "@ant-design/icons";
 import { ParametersOutput, getApiTreeMethods } from "@/services/apiTree";
-import React, { useCallback, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { Collapse } from "antd";
 import { ISidebarPane } from "@dtinsight/molecule/esm/model";
 import Input from "@/components/input";
-import { SearchOutlined } from "@ant-design/icons";
 import { localize } from "@dtinsight/molecule/esm/i18n/localize";
 import styles from "./index.module.scss";
-import { useReactive } from "ahooks";
+import { useRequest } from "ahooks";
 
 const { Panel } = Collapse;
 
 export function SidePaneView() {
-  const state = useReactive({
-    apiFileTree: [],
-    apiFileTreeDetail: {
-      description: "",
-      id: 0,
-    },
-  });
-
   useEffect(() => {
-    (async () => {
-      const { success, data } = await getApiTreeMethods({
-        code: "algorithm-api",
-      });
-      if (success) {
-        state.apiFileTree = data;
-      }
-    })();
+    run({ code: "algorithm-api", apiName: "" });
   }, []);
+
+  const { data, run } = useRequest(getApiTreeMethods, {
+    debounceWait: 1000,
+    manual: true,
+  });
 
   return (
     <div className={styles.file_tree_container}>
@@ -38,9 +27,12 @@ export function SidePaneView() {
         <Input
           prefix={<SearchOutlined />}
           style={{ height: 28, width: 301 }}
-          placeholder="搜索"
+          placeholder={localize("sidePane.search", "搜索API")}
+          allowClear={true}
+          onChange={(e) =>
+            run({ code: "algorithm-api", apiName: e.target.value })
+          }
         />
-        <CloseOutlined style={{ fontSize: "14px" }} />
       </div>
       <Collapse
         expandIcon={({ isActive }) => (
@@ -52,42 +44,50 @@ export function SidePaneView() {
         ghost={true}
         bordered={false}
       >
-        {state.apiFileTree.map((item, index) => (
-          <Panel
-            key={index}
-            header={
-              <div className={styles.file_tree_header_container}>
-                <img className={styles.header_icon} src={item.icon} />
-                <span className={styles.header_text}>{item.apiName}</span>
-              </div>
-            }
-          >
-            <div className={styles.document_details_container}>
-              <p>{item?.description}</p>
-              <div className={styles.parameter_container}>
-                <div className={styles.parameter_item}>
-                  <div className={styles.left_container}>
-                    <p>Name</p>
-                  </div>
-                  <div className={styles.right_container}>
-                    <p>Descipiton</p>
-                  </div>
+        {data?.data ? (
+          data?.data.map((item, index) => (
+            <Panel
+              key={index}
+              header={
+                <div className={styles.file_tree_header_container}>
+                  <img className={styles.header_icon} src={item.icon} />
+                  <span className={styles.header_text}>{item.apiName}</span>
                 </div>
-                {item.inParameters.map((ite: ParametersOutput, idx: number) => (
-                  <div key={idx} className={styles.parameter_item}>
+              }
+            >
+              <div className={styles.document_details_container}>
+                <p>{item?.description}</p>
+                <div className={styles.parameter_container}>
+                  <div className={styles.parameter_item}>
                     <div className={styles.left_container}>
-                      <p>{ite.name}</p>
-                      <a>{ite.typeName}</a>
+                      <p>Name</p>
                     </div>
                     <div className={styles.right_container}>
-                      <p>{ite.description}</p>
+                      <p>Descipiton</p>
                     </div>
                   </div>
-                ))}
+                  {item.inParameters.map(
+                    (ite: ParametersOutput, idx: number) => (
+                      <div key={idx} className={styles.parameter_item}>
+                        <div className={styles.left_container}>
+                          <p>{ite.name}</p>
+                          <a>{ite.typeName}</a>
+                        </div>
+                        <div className={styles.right_container}>
+                          <p>{ite.description}</p>
+                        </div>
+                      </div>
+                    )
+                  )}
+                </div>
               </div>
-            </div>
-          </Panel>
-        ))}
+            </Panel>
+          ))
+        ) : (
+          <div className={styles.empty_container}>
+            <p>{localize("sidePane.noData", "没有此API")}~</p>
+          </div>
+        )}
       </Collapse>
     </div>
   );
