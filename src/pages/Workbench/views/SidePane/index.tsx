a6df1a58603d50ec42e41d75260f9f7d176f4123
Raozhi <2829656235@qq.com>
2022-04-19 18:43:55 +0800

    style: update API document style

--- src/pages/Workbench/views/SidePane/index.tsx
+++ src/pages/Workbench/views/SidePane/index.tsx
@@ -22,7 +22,7 @@ export function SidePaneView() {
   });
 
   return (
-    <div className={styles.file_tree_container}>
+    <>
       <div className={styles.search_container}>
         <Input
           prefix={<SearchOutlined />}
@@ -34,76 +34,78 @@ export function SidePaneView() {
           }
         />
       </div>
-      <Collapse
-        expandIcon={({ isActive }) => (
-          <CaretRightOutlined
-            style={{ color: "#d8d8d8" }}
-            rotate={isActive ? 90 : 0}
-          />
-        )}
-        ghost={true}
-        bordered={false}
-      >
-        {data?.data ? (
-          data?.data.map((item, index) => (
-            <Panel
-              key={index}
-              header={
-                <div className={styles.file_tree_header_container}>
-                  <img className={styles.header_icon} src={item.icon} />
-                  <span className={styles.header_text}>{item.apiName}</span>
-                </div>
-              }
-            >
-              <div className={styles.document_details_container}>
-                <p>{item?.description}</p>
-                <div className={styles.parameter_container}>
-                  <div className={styles.parameter_item}>
-                    <div className={styles.left_container}>
-                      <p>Name</p>
-                    </div>
-                    <div className={styles.right_container}>
-                      <p>Descipiton</p>
-                    </div>
+      <div className={styles.file_tree_container}>
+        <Collapse
+          expandIcon={({ isActive }) => (
+            <CaretRightOutlined
+              style={{ color: "#d8d8d8" }}
+              rotate={isActive ? 90 : 0}
+            />
+          )}
+          ghost={true}
+          bordered={false}
+        >
+          {data?.data ? (
+            data?.data.map((item, index) => (
+              <Panel
+                key={index}
+                header={
+                  <div className={styles.file_tree_header_container}>
+                    <img className={styles.header_icon} src={item.icon} />
+                    <span className={styles.header_text}>{item.apiName}</span>
                   </div>
-                  {item.inParameters.map(
-                    (ite: ParametersOutput, idx: number) => (
-                      <div key={idx} className={styles.parameter_item}>
-                        <div className={styles.left_container}>
-                          <p>{ite.name}</p>
-                          <a>{ite.typeName}</a>
-                        </div>
-                        <div className={styles.right_container}>
-                          <p>{ite.description}</p>
-                        </div>
+                }
+              >
+                <div className={styles.document_details_container}>
+                  <p>{item?.description}</p>
+                  <div className={styles.parameter_container}>
+                    <div className={styles.parameter_item}>
+                      <div className={styles.left_container}>
+                        <p>Name</p>
                       </div>
-                    )
-                  )}
-                  {item.outParameters.map(
-                    (ite: ParametersOutput, idx: number) => (
-                      <div key={idx} className={styles.parameter_item}>
-                        <div className={styles.left_container}>
-                          <p className={styles.return}>Return</p>
-                          <p>{ite.name}</p>
-                          <a>{ite.typeName}</a>
-                        </div>
-                        <div className={styles.right_container}>
-                          <p>{ite.description}</p>
-                        </div>
+                      <div className={styles.right_container}>
+                        <p>Descipiton</p>
                       </div>
-                    )
-                  )}
+                    </div>
+                    {item.inParameters.map(
+                      (ite: ParametersOutput, idx: number) => (
+                        <div key={idx} className={styles.parameter_item}>
+                          <div className={styles.left_container}>
+                            <p>{ite.name}</p>
+                            <a>{ite.typeName}</a>
+                          </div>
+                          <div className={styles.right_container}>
+                            <p>{ite.description}</p>
+                          </div>
+                        </div>
+                      )
+                    )}
+                    {item.outParameters.map(
+                      (ite: ParametersOutput, idx: number) => (
+                        <div key={idx} className={styles.parameter_item}>
+                          <div className={styles.left_container}>
+                            <p className={styles.return}>Return</p>
+                            <p>{ite.name}</p>
+                            <a>{ite.typeName}</a>
+                          </div>
+                          <div className={styles.right_container}>
+                            <p>{ite.description}</p>
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </div>
                 </div>
-              </div>
-            </Panel>
-          ))
-        ) : (
-          <div className={styles.empty_container}>
-            <p>{localize("sidePane.noData", "没有此API")}~</p>
-          </div>
-        )}
-      </Collapse>
-    </div>
+              </Panel>
+            ))
+          ) : (
+            <div className={styles.empty_container}>
+              <p>{localize("sidePane.noData", "没有此API")}~</p>
+            </div>
+          )}
+        </Collapse>
+      </div>
+    </>
   );
 }
 
