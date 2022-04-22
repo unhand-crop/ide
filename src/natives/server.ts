bd24ef1866114098636dcef50e76a8ac4a3149b2
roylin <1002591652@qq.com>
2022-04-22 14:20:35 +0800

    feat: child process server

--- src/natives/server.ts
+++ src/natives/server.ts
@@ -1,33 +1,23 @@
 import { BrowserWindow } from "electron";
 import { fork } from "child_process";
+import getPort from "get-port";
+import { store } from "./store";
 
 export async function initServer(
   mainWindow: BrowserWindow,
   signal: AbortSignal
 ) {
-  fork(__dirname + "/scripts/server.js", [], {
+  const port = await getPort();
+
+  store.set("server-port", port);
+
+  fork(__dirname + "/scripts/server.js", [port.toString()], {
     signal,
   })
-    .on("message", (message) => {
-      console.log("--> !!!", message);
+    .on("message", (data) => {
+      mainWindow.webContents.send("engine-result", data);
     })
     .on("error", () => {
       process.exit(0);
     });
-
-  // const fastify = Fastify({
-  //   logger: true,
-  // });
-  // const port = await getPort();
-  // store.set("server-port", port);
-  // fastify.get("/", (request, reply) => {
-  //   reply.send({ success: true, message: "OK" });
-  // });
-  // fastify.post("/", (request, reply) => {
-  //   mainWindow.webContents.send("engine-result", request.body);
-  //   reply.send({ success: true, message: "OK" });
-  // });
-  // fastify.listen({ port, host: "0.0.0.0" }, (err, address) => {
-  //   if (err) throw err;
-  // });
 }
--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,1 +1,20 @@
-process.send({ foo: "bar" });
+const Fastify = require("fastify");
+
+const port = process.argv[2];
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", (request, reply) => {
+  reply.send({ success: true, message: "OK" });
+});
+
+fastify.post("/", (request, reply) => {
+  process.send(request.body);
+  reply.send({ success: true, message: "OK" });
+});
+
+fastify.listen({ port, host: "0.0.0.0" }, (err, address) => {
+  if (err) throw err;
+});
